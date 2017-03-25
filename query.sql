acb4ef45c87e2be921431c733c39e16d4f3455af
Cody Newman <ctman96@gmail.com>
2017-03-25 16:45:46 -0700

    Queries

--- query.sql
+++ query.sql
@@ -1,7 +1,23 @@
 1)
 
-update Stock
-set quantity = (select s.quantity
-				from Stock s,Employee e
-				where s.BID = e.BID and s.SKU = SKU)
-where SKU = SKU
+UPDATE Stock s
+  SET Quantity = (SELECT s.Quantity
+                  FROM Stock s, Employee e
+                  WHERE s.BID = e.BID AND e.EID = '30000000' AND s.SKU = '10000000')-1
+WHERE s.SKU = '10000000';
+
+
+
+--10) Purchase order
+SELECT Name, SKU, maxquantity-quantity as orderquantity
+FROM Stock s, Product p
+WHERE p.DID = %orderDID AND s.BID = %orderBID
+
+--12) INVENTORY COUNT
+
+--13) SALE REPORT -- TESTED CONFIRMED
+
+SELECT *
+FROM SALE
+WHERE TO_DATE('20161201', 'YYYYMMDD') <= SALEDATE AND
+      SALEDATE <= TO_DATE('20170110', 'YYYYMMDD');

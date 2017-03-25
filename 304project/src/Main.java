bad8c1c09130e145b7269a0df5bfd3cbcb404040
Catherine wu <CatherineWu@shl-wp28.vpl.ca>
2017-03-25 12:23:43 -0700

    java update

--- 304project/src/Main.java
+++ 304project/src/Main.java
@@ -15,7 +15,16 @@ public class Main {
             System.out.println("Creating Connection...");
             con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "ora_o2e9", "a40149122");
             
-            createDatabase(con);
+            System.out.println("Database Create and Populate...");
+            createpopDatabase(con);
+            
+            String SKU = null;
+            String EID = null;
+            String payment = null;
+            String CID = null;
+            //buyProduct(con,SKU,EID,payment,CID);
+            
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }finally{
@@ -29,10 +38,15 @@ public class Main {
 
     }
     
-    public static void createDatabase(Connection con){
+    public static void createpopDatabase(Connection con){
     	Statement stmt = null;
     	try{
+    	System.out.println("Create Statement...");
     	stmt = con.createStatement();
+    	
+    	String sql = "CREATE TABLE Branch";
+    	stmt.executeUpdate(sql);
+    	
     	} catch (SQLException e){
     		e.printStackTrace();
     	} finally{
@@ -43,5 +57,26 @@ public class Main {
     		}
     	}
     	System.out.println("Database Created and Populated");
+    }
+    
+    public static void buyProduct(Connection con, String SKU, String EID, String payment, String CID){
+    	Statement stmt = null;
+    	try{
+    		con.setAutoCommit(false);
+    		System.out.println("Create Statement...");
+    		//String sql = "Select BID from "
+    		//stmt = con.prepareStatement(sql);
+    		
+    	} catch (SQLException e){
+    		e.printStackTrace();
+    	} finally{
+    		try{
+    			if(stmt != null){
+    				stmt.close();
+    				con.setAutoCommit(true);
+    			}
+    		} catch(SQLException se){
+    		}
+    	}
     }
 }

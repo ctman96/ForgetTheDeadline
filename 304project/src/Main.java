85a02e50364aa3e1d169a64425b2991f091353a7
Cody Newman <ctman96@gmail.com>
2017-03-25 17:31:49 -0700

    Finished one query

--- 304project/src/Main.java
+++ 304project/src/Main.java
@@ -1,7 +1,8 @@
 import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
+import java.text.*;
 
 import oracle.jdbc.driver.OracleDriver;
 
@@ -16,12 +17,12 @@ public class Main {
             DriverManager.registerDriver(new OracleDriver());
             
             System.out.println("Creating Connection...");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:ug", "ora_o2e9", "a40149122");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:ug", "ora_k2a0b", "a35833145");
             
-            String SKU = null;
-            String EID = null;
-            String payment = null;
-            String CID = null;
+            String SKU = "10000000";
+            String EID = "30000000";
+            String payment = "CC123123";
+            String CID = "35553916";
             buyProduct(con,SKU,EID,payment,CID);
             
             
@@ -43,35 +44,43 @@ public class Main {
     public static void buyProduct(Connection con, String SKU, String EID, String payment, String CID){
     	PreparedStatement update_stmt = null;
     	PreparedStatement insert_stmt = null;
-    	String update_str = "update Stock s " +
-    						"set quantity = (select s.quantity " +
-    										"from Stock s,Employee e " +
-    										"where s.BID = e.BID and s.SKU = ? and e.EID = ?) - 1" +
-    						"where s.SKU = ?";
-    	String insert_str = "insert into sales values(?, (select max(snum)" +
-    						"from sales) + 1, ?, ?, ?, ?)";
+    	String update_str = "UPDATE Stock s " +
+				"SET Quantity = (SELECT s.Quantity " +
+				"                FROM Stock s, Employee e " +
+				"                WHERE e.EID = ? AND s.BID = e.BID AND s.SKU = ?)-1 " +
+				"WHERE s.SKU = ? AND s.bid = (SELECT s.BID " +
+				"                 FROM Stock s, Employee e " +
+				"                 WHERE e.EID = ? AND s.BID = e.BID AND s.SKU = ?)";
+    	String insert_str = "insert into Sale values(?, ?"+
+    						", ?, ?, ?, ?)";
     	try{
     		con.setAutoCommit(false);
     		System.out.println("Create Statement...");
     		update_stmt = con.prepareStatement(update_str);
     		insert_stmt = con.prepareStatement(insert_str);
     		
-    		update_stmt.setString(1,SKU);
+    		update_stmt.setString(1,EID);
     		update_stmt.setString(2,SKU);
-    		update_stmt.setString(3,EID);
+    		update_stmt.setString(3,SKU);
+			update_stmt.setString(4,EID);
+			update_stmt.setString(5,SKU);
     		update_stmt.executeUpdate();
     		
     		insert_stmt.setString(1, payment);
-    		insert_stmt.setString(2, SKU);
-    		Date date = new Date();
-    		insert_stmt.setDate(3, (java.sql.Date) date);
-    		insert_stmt.setString(4,CID);
-    		insert_stmt.setString(5,EID);
+    		insert_stmt.setString(2, "50000000");
+    		insert_stmt.setString(3, SKU);
+    		String stringDate = new String("18/08/01");
+    		SimpleDateFormat fm = new SimpleDateFormat("dd/MM/yy");
+			java.util.Date utilDate = fm.parse(stringDate);
+    		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+    		insert_stmt.setDate(4,sqlDate);
+			insert_stmt.setString(5,CID);
+    		insert_stmt.setString(6,EID);
     		insert_stmt.executeUpdate();
     		
     		con.commit();
     		
-    	} catch (SQLException e){
+    	} catch (Exception e){
     		e.printStackTrace();
     	} finally{
     		try{

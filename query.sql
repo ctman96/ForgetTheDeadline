9ee31dad01616e8c1eeea1f9ad75e8929afb714e
Cody Newman <cody.newman@alumni.ubc.ca>
2017-03-29 20:06:54 -0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
+--DDL and population can be found in /resorces/sql/
+
 --===================
 --1) Buying a product TESTED CONFIRMED
 --===================
@@ -12,7 +14,7 @@ WHERE s.SKU = ? AND s.bid = (SELECT s.BID
                  WHERE e.EID = ? AND s.BID = e.BID AND s.SKU = ?);
 --?: (1,EID), (2,SKU), (3,SKU), (4,EID), (5,SKU)
 INSERT INTO Sale
-VALUES(?,?,?,?,?,?);
+VALUES(?,?,?,?,?,?)
 --?: (1,payment), (2,generated snum), (3,SKU), (4,Date), (5,CID), (6,EID)
 
 --===================
@@ -20,7 +22,7 @@ VALUES(?,?,?,?,?,?);
 --===================
 --Inputs name, cid, phone, address
 INSERT INTO Customer
- VALUES (?,?,?,?);
+ VALUES (?,?,?,?)
 --?: (1,name), (2,cid), (3,phone), (4,address)
 
 --===================
@@ -28,7 +30,7 @@ INSERT INTO Customer
 --===================
 --Inputs: EID,BID,Wage,Position,Phone,Address
 INSERT INTO Employee
-VALUES(?,?,?,?,?,?);
+VALUES(?,?,?,?,?,?)
 --?: (1,EID),(2,BID),(3,Wage),(4,Position),(5,Phone),(6,Address)
 
 --===================
@@ -36,7 +38,7 @@ VALUES(?,?,?,?,?,?);
 --===================
 --Inputs: EID
 DELETE FROM Employee e
-WHERE e.EID = ?;
+WHERE e.EID = ?
 --?: (1,EID)
 
 --===================
@@ -44,7 +46,7 @@ WHERE e.EID = ?;
 --===================
 --Inputs: SKU,DID,name,price
 INSERT INTO Product
-VALUES(?,?,?,?);
+VALUES(?,?,?,?)
 --?: (1,name),(2,SKU),(3,price),(4,DID)
 
 --===================
@@ -61,7 +63,7 @@ VALUES(?,?,?,?)
 --Inputs: SKU,newPrice
 UPDATE Product p
 SET price = ?
-WHERE p.SKU = ?;
+WHERE p.SKU = ?
 --?: (1,newPrice),(2,SKU)
 
 --===================
@@ -74,7 +76,7 @@ WHERE p.SKU = ?;
 	--?: (1,CID)
 	SELECT Payment, Snum, SKU, saleDate
 	FROM Sale s
-	WHERE s.CID = ? AND s.saleDate >= ?;
+	WHERE s.CID = ? AND s.saleDate >= ?
 	--?: (1,CID),(2,generatedDate)
 --Case Name and Phone
 	SELECT * FROM Customer c WHERE c.Name=? AND c.Phone=?;
@@ -82,7 +84,7 @@ WHERE p.SKU = ?;
 	SELECT Payment, Snum, SKU, saleDate
 	FROM Sale s, Customer c
 	WHERE s.CID=c.CID AND c.Name = ? AND c.Phone=? 
-	AND s.saleDate >= ?;
+	AND s.saleDate >= ?
 	--?: (1,Name),(2,Phone),(3,generatedDate)
 --===================
 --10) Purchase order TESTED CONFIRMED
@@ -93,7 +95,7 @@ SELECT Name, s.SKU, Price,
   Price*(maxquantity - quantity) as orderPrice
 FROM Stock s, Product p
 WHERE p.DID = ? AND s.BID = ?
-      AND s.SKU=p.SKU AND maxquantity-quantity >0;
+      AND s.SKU=p.SKU AND maxquantity-quantity >0
 --?: (1,DID), (2,BID)
 
 --===================
@@ -102,7 +104,7 @@ WHERE p.DID = ? AND s.BID = ?
 --Inputs: BID,SKU,addQuantity
 UPDATE Stock s
 SET quantity = s.quantity+?
-WHERE s.BID=? AND s.SKU=?;
+WHERE s.BID=? AND s.SKU=?
 --?: (1,addQuantity),(2,BID),(3,SKU)
 
 --===================
@@ -111,7 +113,7 @@ WHERE s.BID=? AND s.SKU=?;
 --Inputs: BID
 SELECT Name, s.SKU, Price, Quantity
 FROM Stock s, Product p
-WHERE s.SKU=p.SKU AND s.BID=?;
+WHERE s.SKU=p.SKU AND s.BID=?
 --?: (1,BID)
 
 --===================
@@ -120,34 +122,42 @@ WHERE s.SKU=p.SKU AND s.BID=?;
 --Inputs: startDate, endDate
 SELECT *
 FROM Sale
-WHERE ? <= SALEDATE AND SALEDATE <= ?;
+WHERE ? <= SALEDATE AND SALEDATE <= ?
 --?: (1,startDate), (2,endDate)
 
 --===================
 --13) SALE REPORT - BEST EMPLOYEES -- AGGREGATION -- TESTED CONFIRMED
 --===================
---Inputs: startDate, endDate
-SELECT e.EID, COUNT(e.EID)
+--Inputs: startDate, endDate,
+--optional: AGGREGATE
+
+--SELECT AGGREGATE(count) FROM(
+SELECT e.EID, COUNT(e.EID) count
 FROM Sale s, Employee e
 WHERE s.eid = e.eid 
 	AND ? <= SALEDATE AND SALEDATE <= ?
 GROUP BY e.EID
-ORDER BY COUNT(e.EID) DESC;
+ORDER BY COUNT(e.EID) DESC
+--)
 --(1,startDate), (2,endDate)
 
 --Aggregation query: Pick one query that requires the use of aggregation (min, max, average, or count 
 --are all fine). Rerun with at least one other example of aggregation.
 
 --===================
---14) SALE REPORT - BEST SELLING PRODUCTS BY BRANCH -- NESTED AGGREGATION -- TESTED CONFIRMED
+--14) SALE REPORT - PRODUCT&BRANCH -- NESTED AGGREGATION -- TESTED CONFIRMED
 --===================
 --Inputs: startDate, endDate
-SELECT SKU, BID, COUNT(SKU)
+--optional: AGGREGATE
+
+--SELECT BID, AGGREGATE(count) FROM (
+SELECT SKU, BID, COUNT(SKU) count
 FROM Sale s, Employee e
 WHERE ? <= SALEDATE AND SALEDATE <= ?
 	AND s.eid = e.eid
 GROUP BY SKU, BID
-ORDER BY BID, COUNT(SKU) DESC;
+ORDER BY BID, COUNT(SKU) DESC
+--)
 --?: (1,startDate), (2,endDate)
 
 --Nested aggregation with group-by: Pick one query that finds some aggregated value for each group 
@@ -170,3 +180,46 @@ WHERE NOT EXISTS
 --query (eg. find all the customers who bought all the items). Prove that your division results would 
 --change based on the data in your database. You can do it either by inserting a new tuple into or 
 --deleting an existing tuple from the appropriate tables.
+
+
+
+
+
+
+--TRIGGERS:
+--were hardcoded rather than scripted because our parser couldn't handle it
+CREATE OR REPLACE TRIGGER branch_on_insert
+BEFORE INSERT ON Branch
+FOR EACH ROW
+BEGIN
+:new.BID := trim(to_char(branch_sequence.nextval, '00000000'));
+END;
+
+CREATE OR REPLACE TRIGGER employee_on_insert
+BEFORE INSERT ON Employee
+FOR EACH ROW
+BEGIN
+:new.EID := trim(to_char(employee_sequence.nextval, '00000000'));+
+END;
+
+CREATE OR REPLACE TRIGGER developer_on_insert
+BEFORE INSERT ON Developer
+FOR EACH ROW
+BEGIN
+:new.DID := trim(to_char(developer_sequence.nextval, '00000000'));
+END;
+
+CREATE OR REPLACE TRIGGER customer_on_insert
+BEFORE INSERT ON Customer
+FOR EACH ROW
+BEGIN
+:new.CID := trim(to_char(customer_sequence.nextval, '00000000'));
+END;
+
+CREATE OR REPLACE TRIGGER sale_on_insert
+BEFORE INSERT ON Sale
+FOR EACH ROW
+BEGIN
+:new.snum := trim(to_char(sale_sequence.nextval, '00000000'));
+END;
+

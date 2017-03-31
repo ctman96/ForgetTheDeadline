857f0c9c07977eb3435ee59c2b437d239f21a18b
Cody Newman <ctman96@gmail.com>
2017-03-30 21:32:12 -0700

    Add Parser Exception Error Handling

--- src/sql/SQLUtil.java
+++ src/sql/SQLUtil.java
@@ -1,12 +1,13 @@
 package sql;
 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
-public class SQLUtil {
 
+public class SQLUtil {
     /**
      * Executes statements from a file
      * @param connection database connection
@@ -15,6 +16,8 @@ public class SQLUtil {
     public static void executeFile(Connection connection, File file) throws IOException, SQLException {
         String fileName = file.getName();
         int statementCount = 0;
+        int failureCount = 0;
+        String failures="";
 
         System.out.printf("Executing file %s.\n", fileName);
         try (FileReader fReader = new FileReader(file);
@@ -35,15 +38,23 @@ public class SQLUtil {
                 }
 
                 Statement statement = connection.createStatement();
-                statement.execute(trimString);
-                statementCount++;
-
+                try {
+                    statement.execute(trimString);
+                    statementCount++;
+                }catch (SQLException e){
+                    failures += "\n On line:"+trimString+e.getMessage()+"\n";
+                    failureCount++;
+                }
             }
 
-            System.out.printf("%d statements executed from file %s.\n", statementCount, fileName);
+            System.out.printf("%d statements executed, %d exceptions from file %s.\n", statementCount,failureCount, fileName);
         } catch (SQLException e) {
             System.out.printf("%d statements executed from file %s before exception.\n", statementCount, fileName);
             throw e;
+        }
+        if(!failures.equals("")){
+            failures = "Encountered "+failureCount+" exception(s) during execution. \n"+failures;
+            throw new SQLException(failures);
         }
     }
 
